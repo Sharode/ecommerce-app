fe9387b0eacb15f553fbb725a4832cbf6f59315c
Neil Williams <Williams.Sharode@gmail.com>
2020-02-07 19:30:44 -0500

    reconstructed a new dataset for interfaces

--- src/ components/Products.js
+++ src/ components/Products.js
@@ -1,24 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import PropTypes from 'prop-types'
 
 function Products({ productItems, handleAddToCart }) {
+
+    const [hovering, setHovering] = useState('hidden')
+
+    const handleMouseEnter = (e, product) => {
+        console.log(product.added === key)
+        console.log(key)
+        console.log(product.added)
+        if (product.added === e.target.id) {
+            setHovering(prevState => prevState = 'block')
+        }
+    }
+
+    const handleMouseLeave = () => {
+        setHovering(prevState => prevState = 'hidden')
+    }
+
+
     const products = productItems.map(product => (
-        < div className="w-1/4 m-4" key={product.added} >
+        < div className="w-1/5 m-4" key={product.added} value={product.added}
+            onMouseEnter={(e) => handleMouseEnter(e, product)}
+            onMouseLeave={handleMouseLeave}
+        >
 
             <div class="max-w-sm rounded overflow-hidden shadow-md">
                 <div class="px-6 py-4 image-bg">
                     <img className="h-full w-full object-cover" src={`../../../products/${product.productImg}`} alt={product.name} />
 
                 </div>
-                <div className="px-6 py-4">
-                    <div className="font-semibold text-base mb-2">{product.name}</div>
-
-                    <span className="inline-block bg-gray-200 rounded-full px-1 py-1 text-sm font-semibold text-gray-700 mr-2">${product.price}</span>
-                    <button
-                        onClick={(e) => handleAddToCart(e, product)}
-                        className="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-semibold text-black mr-2">Add to Cart </button>
+                <div className="px-4 py-2 text-center">
+                    <div className="font-semibold text-base mb-2">
+                        {product.name}
+                    </div>
+                    <div>
+                        <span className="inline-block px-1 py-1 text-sm font-semibold text-gray-700 mr-2">${product.price}</span>
+                    </div>
                 </div>
+                <div
+
+                    onClick={(e) => handleAddToCart(e, product)}
+                    className={`${hovering} inline-block bg-yellow-500 w-full px-3 py-1 text-sm cursor-pointer text-center font-semibold text-black mr-2`}
+
+                >{hovering === "hidden" ? "" : "Add to Cart"} </div>
             </div>
 
         </div >
